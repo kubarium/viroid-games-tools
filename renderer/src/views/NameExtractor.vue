34d4a4aac27e2a906ec5a65118102d6ee686f992
kubarium <iam@denizkumsal.com>
2018-11-05 00:40:45 -0500

    tests npm-run-all

--- renderer/src/views/NameExtractor.vue
+++ renderer/src/views/NameExtractor.vue
@@ -21,72 +21,72 @@
           <div class="control">
             <input type="text" class="input book-name" v-tooltip.top="$store.state.file.realname||'Choose a file'" v-model="$store.state.file.name">
           </div>
-            <div class="control">
-              <button class="button is-primary" :class="{'is-loading':isLoading}" :disabled="isLoading || $store.state.file.name=='' || $store.state.file.realname==''" @click="extract">
-                Process
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <hr>
-      <div class="container is-fullhd">
-        <div class="columns">
-          <div class="column">
-            <div class="panel">
-              <div class="panel-heading">Detected Names
-                <span v-show="$store.state.detectedNames.length">({{$store.state.detectedNames.length}})</span>
-              </div>
-              <div class="panel-block tags">
-
-                <span class="tag" :class="{'is-info':!item.processed,'is-success':item.processed}" v-for="(item,index) in $store.state.detectedNames" :key="item.label" @click="$store.dispatch('findPassage',item.label)">{{item.label}}
-                  <button class="delete is-small" @click="$store.commit('deleteDetectedName',index)"></button>
-                </span>
-
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Workbench</label>
-
-              <tabs type="boxed">
-                <tab-pane label="Description" selected>Description</tab-pane>
-                <tab-pane label="Tags">Tags</tab-pane>
-              </tabs>
-            </div>
-          </div>
-          <div class="column">
-            <div class="panel">
-              <div class="panel-heading">
-                <div class="level">
-                  <div class="level-left">
-                    <span>
-                      Passages&nbsp;
-                    </span>
-                    <span v-show="numOfPassages">with {{$store.state.passagesFor}} ({{numOfPassages}})</span>
-                  </div>
-                  <div class="level-right">
-                    <slider v-show="numOfPassages>5*pageSize" class="pulled-right" type="info" :value="page" :max="Math.floor(numOfPassages/pageSize)" :step="1" @change="revealPassages"></slider>
-                    <nav class="pagination is-small" role="navigation" aria-label="pagination" v-show="numOfPassages<=5*pageSize">
-                      <ul class="pagination-list">
-                        <li>
-                          <a class="pagination-link" :class="{'is-current':page==pagination-1}" v-for="pagination in Math.ceil(numOfPassages/pageSize)" :key="pagination" @click="revealPassages(pagination-1)">{{pagination}}</a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                </div>
-              </div>
-              <div class="panel-block" v-for="(passage,index) in $store.state.passages.slice(page*pageSize,page*pageSize+pageSize)" :key="index">
-                <p v-html="passage" class="passage"></p>
-                <!-- <p>
-                </p> -->
-              </div>
-              <div class="panel-block" v-show="numOfPassages===0">Select a detected name to see where it's mentioned in the book</div>
-            </div>
+          <div class="control">
+            <button class="button is-primary" :class="{'is-loading':isLoading}" :disabled="isLoading || $store.state.file.name=='' || $store.state.file.realname==''" @click="extract">
+              Process
+            </button>
           </div>
         </div>
       </div>
     </div>
+    <hr>
+    <div class="container is-fullhd">
+      <div class="columns">
+        <div class="column">
+          <div class="panel">
+            <div class="panel-heading">Detected Names
+              <span v-show="$store.state.detectedNames.length">({{$store.state.detectedNames.length}})</span>
+            </div>
+            <div class="panel-block tags">
+
+              <span class="tag" :class="{'is-info':!item.processed,'is-success':item.processed}" v-for="(item,index) in $store.state.detectedNames" :key="item.label" @click="$store.dispatch('findPassage',item.label)">{{item.label}}
+                <button class="delete is-small" @click="$store.commit('deleteDetectedName',index)"></button>
+              </span>
+
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Workbench</label>
+
+            <tabs type="boxed">
+              <tab-pane label="Description" selected>Description</tab-pane>
+              <tab-pane label="Tags">Tags</tab-pane>
+            </tabs>
+          </div>
+        </div>
+        <div class="column">
+          <div class="panel">
+            <div class="panel-heading">
+              <div class="level">
+                <div class="level-left">
+                  <span>
+                    Passages&nbsp;
+                  </span>
+                  <span v-show="numOfPassages">with {{$store.state.passagesFor}} ({{numOfPassages}})</span>
+                </div>
+                <div class="level-right">
+                  <slider v-show="numOfPassages>5*pageSize" class="pulled-right" type="info" :value="page" :max="Math.floor(numOfPassages/pageSize)" :step="1" @change="revealPassages"></slider>
+                  <nav class="pagination is-small" role="navigation" aria-label="pagination" v-show="numOfPassages<=5*pageSize">
+                    <ul class="pagination-list">
+                      <li>
+                        <a class="pagination-link" :class="{'is-current':page==pagination-1}" v-for="pagination in Math.ceil(numOfPassages/pageSize)" :key="pagination" @click="revealPassages(pagination-1)">{{pagination}}</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            </div>
+            <div class="panel-block" v-for="(passage,index) in $store.state.passages.slice(page*pageSize,page*pageSize+pageSize)" :key="index">
+              <p v-html="passage" class="passage"></p>
+              <!-- <p>
+                </p> -->
+            </div>
+            <div class="panel-block" v-show="numOfPassages===0">Select a detected name to see where it's mentioned in the book</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
